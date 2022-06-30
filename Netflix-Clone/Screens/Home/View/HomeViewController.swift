e94476e1773dbbdba5c49ee9251583f78858bb30
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-30 11:28:24 +0700

    feat(home): setup homeFeedTable

--- Netflix-Clone/Screens/Home/View/HomeViewController.swift
+++ Netflix-Clone/Screens/Home/View/HomeViewController.swift
@@ -8,15 +8,45 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    // MARK: Init
+    // MARK: Properties
+
+    private let homeFeedTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+
+    // MARK: Init & Override func.
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setupUI()
+        setup()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
     }
 
     // MARK: Methods
 
-    func setupUI() {}
+    func setup() {
+        view.addSubview(homeFeedTable)
+
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello world!"
+        return cell
+    }
 }
