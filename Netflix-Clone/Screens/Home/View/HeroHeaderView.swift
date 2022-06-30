4213d008a9ce5fd21e6d2ca89b219d6093d63310
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-30 12:40:45 +0700

    feat(home): create HeroHeaderView class

---
+++ Netflix-Clone/Screens/Home/View/HeroHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  HeroHeaderView.swift
+//  Netflix-Clone
+//
+//  Created by 7Peaks on 30/6/2565 BE.
+//
+
+import UIKit
+
+class HeroHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
