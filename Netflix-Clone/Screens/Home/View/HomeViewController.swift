edc16239e26549cb9ee82fce7ce0aa6fe9e3c1ac
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-30 10:55:37 +0700

    feat(main): setup tab bar

--- Netflix-Clone/Screens/Home/View/HomeViewController.swift
+++ Netflix-Clone/Screens/Home/View/HomeViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    // MARK: Init
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupUI()
     }
+
+    // MARK: Methods
+
+    func setupUI() {}
 }
--- Netflix-Clone/Screens/MainTabBar/MainTabBarViewController.swift
+++ Netflix-Clone/Screens/MainTabBar/MainTabBarViewController.swift
@@ -8,13 +8,42 @@
 import UIKit
 
 class MainTabBarViewController: UITabBarController {
+    // MARK: Init
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupUI()
+        setup()
+    }
+
+    // MARK: Methods
+
+    func setupUI() {
         view.backgroundColor = .systemYellow
     }
 
+    func setup() {
+        // declare VC properties
+        let homeVC = UINavigationController(rootViewController: HomeViewController())
+        let upcomingVC = UINavigationController(rootViewController: UpcomingViewController())
+        let searchVC = UINavigationController(rootViewController: SearchViewController())
+        let downloadVC = UINavigationController(rootViewController: DownloadsViewController())
 
+        // set tabBarItem
+        homeVC.tabBarItem.image = UIImage(systemName: "house")
+        upcomingVC.tabBarItem.image = UIImage(systemName: "play.circle")
+        searchVC.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        downloadVC.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+
+        // set title
+        homeVC.title = "Home"
+        upcomingVC.title = "Coming Soon"
+        searchVC.title = "Top Search"
+        downloadVC.title = "Downloads"
+
+        tabBar.tintColor = .label
+
+        setViewControllers([homeVC, upcomingVC, searchVC, downloadVC], animated: true)
+    }
 }
-
