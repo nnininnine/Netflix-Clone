b98e55175b23838038b9eba1f1cd23ff264447f9
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-30 10:26:14 +0700

    feat: create DownloadsViewController class

---
+++ Netflix-Clone/Screens/Downloads/View/DownloadsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DownloadsViewController.swift
+//  Netflix-Clone
+//
+//  Created by 7Peaks on 30/6/2565 BE.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
