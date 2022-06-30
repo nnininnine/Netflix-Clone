69edce8700f7bb6ff223e1949986254d488621b5
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-30 17:15:35 +0700

    feat(home): change manual hide navbar to hidesBarsOnSwipe

--- Netflix-Clone/Screens/Home/View/HomeViewController.swift
+++ Netflix-Clone/Screens/Home/View/HomeViewController.swift
@@ -51,6 +51,8 @@ class HomeViewController: UIViewController {
             UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
         ]
         navigationController?.navigationBar.tintColor = .white
+
+        navigationController?.hidesBarsOnSwipe = true
     }
 }
 
@@ -77,12 +79,5 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let defaultOffset = view.safeAreaInsets.top
-        let offset = scrollView.contentOffset.y + defaultOffset
-        
-        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
     }
 }
