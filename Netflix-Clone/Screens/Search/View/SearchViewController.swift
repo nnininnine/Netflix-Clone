dd3faa3bfab8793a42957899cabb6846340a398e
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-30 10:24:15 +0700

    feat: create SearchViewController class

---
+++ Netflix-Clone/Screens/Search/View/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  Netflix-Clone
+//
+//  Created by 7Peaks on 30/6/2565 BE.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
