9c5515d8b1826d396679feb8e617ef0feb7e3b90
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-01 12:23:56 +0700

    feat(home): implement PopularMoviesResponse model

--- Netflix-Clone/Screens/Home/Model/PopularMovie.swift
+++ Netflix-Clone/Screens/Home/Model/PopularMovie.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+typealias PopularMovies = [PopularMovie]
+
+struct PopularMoviesResponse: Codable {
+    let page: Int
+    let totalResult: Int
+    let totalPages: Int
+    let results: PopularMovies
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalResult = "total_result"
+        case totalPages = "total_pages"
+    }
+}
+
+struct PopularMovie: Codable {
+    let posterPath: String?
+    let adult: Bool
+    let overview: String
+    let releaseDate: String
+    let genreIds: [Int]
+    let id: Int
+    let originalTitle: String
+    let originalLanguage: String
+    let title: String
+    let backdropPath: String?
+    let popularity: Double
+    let voteCount: Int
+    let video: Bool
+    let voteAverage: Double
+
+    enum CodingKeys: String, CodingKey {
+        case adult, overview, id, title, popularity, video
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case genreIds = "genre_ids"
+        case originalTitle = "original_title"
+        case originalLanguage = "original_language"
+        case backdropPath = "backdrop_path"
+        case voteCount = "vote_count"
+        case voteAverage = "vote_average"
+    }
+}
