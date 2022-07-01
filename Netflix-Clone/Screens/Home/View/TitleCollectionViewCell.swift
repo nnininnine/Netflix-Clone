a169efdb7692dddaf5a57cb2d20cc6cd2b2e80f3
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-01 16:23:04 +0700

    feat(home): create TitleCollectionViewCell class

---
+++ Netflix-Clone/Screens/Home/View/TitleCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix-Clone
+//
+//  Created by Nuttapon Buaban on 1/7/2565 BE.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    // MARK: Properties
+
+    static let identifier = "TitleCollectionViewCell"
+}
