56563e496fc20f2d50fdbfe84a27bd3a2becd41b
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-01 17:12:27 +0700

    feat: add base image url constant

--- Netflix-Clone/Utilities/Constants.swift
+++ Netflix-Clone/Utilities/Constants.swift
@@ -10,11 +10,13 @@ import Foundation
 enum ConstantsKeys: String {
     case BASE_URL
     case API_KEY
+    case BASE_IMG_URL
 }
 
 enum Constants {
     static let baseUrl: String = getConstantFromInfo(forKey: ConstantsKeys.BASE_URL.rawValue, type: "") ?? ""
     static let apiKey: String = getConstantFromInfo(forKey: ConstantsKeys.API_KEY.rawValue, type: "") ?? ""
+    static let baseImgUrl: String = getConstantFromInfo(forKey: ConstantsKeys.BASE_IMG_URL.rawValue, type: "") ?? ""
 }
 
 private func getConstantFromInfo<T>(forKey key: String, type: T) -> T? {
