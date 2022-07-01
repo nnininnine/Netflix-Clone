30107f5983cdeb793ac7883c0c2a9bbeac7817ef
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-01 11:13:46 +0700

    feat: implement Constants enum

--- Netflix-Clone/Utilities/Constants.swift
+++ Netflix-Clone/Utilities/Constants.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+enum ConstantsKeys: String {
+    case BASE_URL
+    case API_KEY
+}
+
+enum Constants {
+    static let baseUrl: String = getConstantFromInfo(forKey: ConstantsKeys.BASE_URL.rawValue, type: "") ?? ""
+    static let apiKey: String = getConstantFromInfo(forKey: ConstantsKeys.API_KEY.rawValue, type: "") ?? ""
+}
+
+private func getConstantFromInfo<T>(forKey key: String, type: T) -> T? {
+    guard let result = Bundle.main.infoDictionary?[key] as? T else { return nil }
+
+    return result
+}
