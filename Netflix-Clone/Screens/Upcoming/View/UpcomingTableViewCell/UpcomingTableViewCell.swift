4e8d21b5750d20665a4058d84ebbd1fc79de5d3e
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-04 15:38:15 +0700

    feat(upcoming): create UpcomingTableViewCell class

---
+++ Netflix-Clone/Screens/Upcoming/View/UpcomingTableViewCell/UpcomingTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  UpcomingTableViewCell.swift
+//  Netflix-Clone
+//
+//  Created by Nuttapon Buaban on 4/7/2565 BE.
+//
+
+import UIKit
+
+class UpcomingTableViewCell: UITableViewCell {
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
