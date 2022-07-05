f94f10b7e316bb6b2968ce0b12402642062dc116
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-05 16:01:51 +0700

    feat(detail): create MovieDetailViewController class

---
+++ Netflix-Clone/Screens/MovieDetail/View/MovieDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieDetailViewController.swift
+//  Netflix-Clone
+//
+//  Created by Nuttapon Buaban on 5/7/2565 BE.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
