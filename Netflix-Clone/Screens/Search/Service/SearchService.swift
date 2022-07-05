a84b4c24a17aa31edebf3fc7985b5b54905564bb
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-05 10:10:33 +0700

    feat(search): create SearchService class

---
+++ Netflix-Clone/Screens/Search/Service/SearchService.swift
@@ -0,0 +1,18 @@
+//
+//  SearchService.swift
+//  Netflix-Clone
+//
+//  Created by Nuttapon Buaban on 5/7/2565 BE.
+//
+
+import Foundation
+
+class SearchService {
+    // MARK: Init
+
+    init() {}
+
+    // MARK: Methods
+
+    func getDiscoverMovies() {}
+}
