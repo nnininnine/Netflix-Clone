ad5e7c779ce171050a44e40af0372bf77af779c1
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-05 16:13:58 +0700

    feat(detail): create MovieDetailViewModel class

---
+++ Netflix-Clone/Screens/MovieDetail/ViewModel/MovieDetailViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  MovieDetailViewModel.swift
+//  Netflix-Clone
+//
+//  Created by Nuttapon Buaban on 5/7/2565 BE.
+//
+
+import Foundation
+
+class MovieDetailViewModel {
+    // MARK: Properties
+
+    final var id: Int
+
+    // MARK: Init
+
+    init() {
+        id = 0
+    }
+
+    init(_ id: Int) {
+        self.id = id
+    }
+
+    // MARK: Methods
+}
