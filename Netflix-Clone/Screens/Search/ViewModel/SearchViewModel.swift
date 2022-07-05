f2c24a18251e0731dba81f741bf547cf291dea97
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-07-05 09:55:55 +0700

    feat(search): create SearchViewModel class

---
+++ Netflix-Clone/Screens/Search/ViewModel/SearchViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewModel.swift
+//  Netflix-Clone
+//
+//  Created by Nuttapon Buaban on 5/7/2565 BE.
+//
+
+import Foundation
+
+class SearchViewModel {
+    // MARK: Properties
+
+    // MARK: Init
+
+    init() {}
+
+    // MARK: Methods
+}
